d22e9a50c47171bee5e58018dcd3168db8ef1a30
Sarthak Saxena <sarthaksaxena1203@gmail.com>
2026-09-16 14:53:41 +0530

    Minor fix

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='markdown-engine-pro',
-    version='2.0.0',
+    version='1.0.0',
     description='A highly scalable, extensible Markdown-to-HTML parser using only standard library',
-    author='Antigravity',
+    author='SS',
     py_modules=['main', 'parser', 'rules'],
     entry_points={
         'console_scripts': [
